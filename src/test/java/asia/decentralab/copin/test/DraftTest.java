f6dbd5a0a5aee624e8e809bc5b30791ce9120d29
hongquan <quanhoangkk01@gmail.com>
2024-06-17 10:00:51 +0700

    move code to new brand

--- src/test/java/asia/decentralab/copin/test/DraftTest.java
+++ src/test/java/asia/decentralab/copin/test/DraftTest.java
@@ -1,2 +1,20 @@
-package asia.decentralab.copin.test;public class DraftTest {
+package asia.decentralab.copin.test;
+
+import asia.decentralab.copin.config.Constant;
+import asia.decentralab.copin.data.ProtocolData;
+import asia.decentralab.copin.utils.APIUtils;
+import asia.decentralab.copin.utils.JsonUtils;
+import org.testng.annotations.Test;
+
+public class DraftTest {
+    @Test(description = "Test Call API")
+    public void callApi(){
+        ProtocolData protocolData = JsonUtils.readJsonFile(Constant.GMX_DATA_FILE_PATH, ProtocolData.class);
+        try {
+            String response = APIUtils.sendRequest(protocolData);
+            System.out.println("API response: " + response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
