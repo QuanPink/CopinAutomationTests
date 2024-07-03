fc0c2ae25c65b51ad9dec07ab6efe2cd998e5557
hongquan <quanhoangkk01@gmail.com>
2024-07-03 10:38:36 +0700

    fix api function

--- src/test/java/asia/decentralab/copin/test/DraftTest.java
+++ src/test/java/asia/decentralab/copin/test/DraftTest.java
@@ -3,11 +3,15 @@ package asia.decentralab.copin.test;
 import asia.decentralab.copin.browser.Driver;
 import asia.decentralab.copin.config.Constant;
 import asia.decentralab.copin.config.DeFiWallets;
+import asia.decentralab.copin.data.ProtocolData;
 import asia.decentralab.copin.data.enumdata.WalletType;
+import asia.decentralab.copin.model.Positions;
 import asia.decentralab.copin.pages.BasePage;
 import asia.decentralab.copin.pages.ConnectWalletPage;
+import asia.decentralab.copin.utils.APIUtils;
 import asia.decentralab.copin.utils.JsonUtils;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DraftTest extends BaseTest {
@@ -33,26 +37,22 @@ public class DraftTest extends BaseTest {
     }
 
 //    @Test(description = "Test Call API")
-//    public void callApi(){
+//    public void callApi() {
 //        ProtocolData protocolData = JsonUtils.readJsonFile(Constant.GMX_DATA_FILE_PATH, ProtocolData.class);
-//        try {
-//            String response = APIUtils.sendRequest(protocolData);
-//            System.out.println("API response: " + response);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+//        String response = APIUtils.sendRequest(protocolData);
+//        System.out.println("API response: " + response);
 //    }
-
+//
 //    @DataProvider(name = "listOfTrader")
-//    public Object[][] dpMethod(){
+//    public Object[][] dpMethod() {
 //        Positions positions = JsonUtils.readJsonFile(Constant.POSITIONS_FILE_PATH, Positions.class);
 //        Positions.Position closedPosition = positions.getClosePosition();
 //        Positions.Position liquidatePosition = positions.getLiquidatePosition();
-//        return new Object[][] {{closedPosition}, {liquidatePosition}};
+//        return new Object[][]{{closedPosition}, {liquidatePosition}};
 //    }
 //
-//    @Test (dataProvider = "listOfTrader")
-//    public void myTest (Positions.Position position) {
+//    @Test(dataProvider = "listOfTrader")
+//    public void myTest(Positions.Position position) {
 //        System.out.println("Tx Hash of the position : " + position.getTxHash());
 //    }
 }
